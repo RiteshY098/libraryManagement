28a8d9647e7ce8ec3f8f5fd461b68c528fb02d3c
nightwing098 <86614628+nightwing098@users.noreply.github.com>
2023-10-16 20:33:40 +0530

    Update Controller.java

--- librarymanagementsystem/src/edu/qsp/lms/controller/Controller.java
+++ librarymanagementsystem/src/edu/qsp/lms/controller/Controller.java
@@ -7,51 +7,51 @@ import edu.qsp.lms.model.Book;
 import edu.qsp.lms.model.Library;
 
 public class Controller {
-	private Library library = new Library();
+	private Library library = new Library(); // create a library object
 
 	public void addBook(Book book) {
-		List<Book> books = library.getBooks();
-		if (books == null) {
+		List<Book> books = library.getBooks(); // get the list of books from the library
+		if (books == null) { // if the list is null, create a new list
 			books = new ArrayList<Book>();
-			library.setBooks(books);
+			library.setBooks(books); // set the list to the library
 		}
-		books.add(book);
+		books.add(book); // add the book to the list
 	}
 
 	public Book searchBook(String bookName) {
-		List<Book> books = library.getBooks();
+		List<Book> books = library.getBooks(); // get the list of books from the library
 		//
-		Iterator<Book> iterator = books.iterator();
-		while (iterator.hasNext()) {
-			Book book = iterator.next();
-			if (book.getBookName().equalsIgnoreCase(bookName)) {
-				return book;
+		Iterator<Book> iterator = books.iterator(); // create an iterator to traverse the list
+		while (iterator.hasNext()) { // while there is a next element in the list
+			Book book = iterator.next(); // get the next book
+			if (book.getBookName().equalsIgnoreCase(bookName)) { // if the book name matches the given name, ignoring case
+				return book; // return the book
 			}
 		}
-		return null;
+		return null; // if no book is found, return null
 	}
 
 	public boolean removeBook(String bookName) {
-		Book book = searchBook(bookName);
-		if (book != null) {
-			List<Book> books = library.getBooks();
-			books.remove(book);
-			return true;
+		Book book = searchBook(bookName); // search for the book by name
+		if (book != null) { // if the book is found
+			List<Book> books = library.getBooks(); // get the list of books from the library
+			books.remove(book); // remove the book from the list
+			return true; // return true to indicate success
 		}
-		return false;
+		return false; // if the book is not found, return false to indicate failure
 	}
 
 	public boolean updateBookPriceByBookName(Book refBook) {
-		Book book = searchBook(refBook.getBookName());
-		if (book != null) {
-			book.setBookPrice(refBook.getBookPrice());
-			return true;
+		Book book = searchBook(refBook.getBookName()); // search for the book by name
+		if (book != null) { // if the book is found
+			book.setBookPrice(refBook.getBookPrice()); // update the book price with the given price
+			return true; // return true to indicate success
 		}
-		return false;
+		return false; // if the book is not found, return false to indicate failure
 	}
 
 	public List<Book> getAllBooks() {
-		return library.getBooks();
+		return library.getBooks(); // return the list of books from the library
 
 	}
 
